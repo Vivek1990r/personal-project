ff02e6a90819f066038920a300c9405d928653e0
Vivek1990r <vivekanandareddyr1990@gmail.com>
2025-04-25 14:36:00 +0000

    Final project updates

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,26 @@
+import { skeleton } from '../../utils';
+
+const Footer = ({
+  content,
+  loading,
+}: {
+  content: string | null;
+  loading: boolean;
+}) => {
+  if (!content) return null;
+
+  return (
+    <div className="card-body">
+      {loading ? (
+        skeleton({ widthCls: 'w-52', heightCls: 'h-6' })
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      )}
+    </div>
+  );
+};
+
+export default Footer;
+
+
+// TODO: Consider refactoring for better performance
